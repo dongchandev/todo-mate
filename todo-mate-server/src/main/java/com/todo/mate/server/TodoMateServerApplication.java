940516739c2e51ce4f7dc88041ae3a9a31ecb186
dongchandev <dc4653.dev@gmail.com>
2025-11-03 14:38:07 +0900

    init: web & server project setup

---
+++ todo-mate-server/src/main/java/com/todo/mate/server/TodoMateServerApplication.java
@@ -0,0 +1,13 @@
+package com.todo.mate.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMateServerApplication.class, args);
+    }
+
+}
---
+++ todo-mate-server/src/test/java/com/todo/mate/server/TodoMateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.mate.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
